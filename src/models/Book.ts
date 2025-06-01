e13dd18cb79365d9f2d93651de9234af747e3ac4
ImKaiserG <sebastiangalvan947@gmail.com>
2025-05-31 22:21:00 -0500

    Update Book.ts

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,1 +1,20 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface Book extends Document {
+  title: string;
+  author: string;
+  description?: string;
+  user: Schema.Types.ObjectId;
+}
+
+const bookSchema = new Schema<Book>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: String,
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+});
+
+export default model<Book>('Book', bookSchema);
 

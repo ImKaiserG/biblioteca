6ba2d2e29de57a92186203b8a959ee791e5c7bf0
ImKaiserG <sebastiangalvan947@gmail.com>
2025-05-31 22:22:16 -0500

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,14 @@
+import { Schema, model, Document } from 'mongoose';
+
+interface User extends Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<User>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+export default model<User>('User', userSchema);
 

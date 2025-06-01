568715a21ca076528d20e4ecba8bd998bf8c2358
ImKaiserG <sebastiangalvan947@gmail.com>
2025-05-31 22:21:43 -0500

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,1 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export interface CustomRequest extends Request {
+  user?: any;
+}
+
+export const verifyToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+
+  if (!token) return res.status(401).json({ message: 'Acceso denegado' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Token inválido' });
+  }
+};
 

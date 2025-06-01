401de40bdb8ed2b78ec43268d4dc9cb4bee79d4a
ImKaiserG <sebastiangalvan947@gmail.com>
2025-05-31 22:25:36 -0500

    Update generateToken.ts

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,1 +1,7 @@
-// Archivo inicial
+export const generateToken = (userId: string): string => {
+  const secret = process.env.JWT_SECRET || 'secretoTemporal';
+  return jwt.sign({ id: userId }, secret, {
+    expiresIn: '7d',
+  });
+};
+
